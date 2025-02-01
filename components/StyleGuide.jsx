157993130654512fb91dc24a0f2150b4af2b1dc3
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-02-01 20:20:45 -0300

    Adicionando componentes na home

--- components/StyleGuide.jsx
+++ components/StyleGuide.jsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
 
 const StyleGuide = () => {
   return (
@@ -22,6 +24,22 @@ const StyleGuide = () => {
         <Button variant="default">Button 1</Button>
         <Button variant="orange">Button 2</Button>
         <Button variant="input">Button 3</Button>
+        <Button variant="orange" size="sm">
+          Button 3
+        </Button>
+        {/* label & input */}
+        <div>
+          <Label htmlFor="firstname">First Name</Label>
+          <Input type="firstname" id="firstname" />
+        </div>
+        <div>
+          <Label htmlFor="lastname">Last Name</Label>
+          <Input type="lastname" id="lastname" />
+        </div>
+        {/* calendar */}
+        <div>
+          <Label>Date</Label>
+        </div>
       </div>
     </>
   );
--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -5,7 +5,7 @@ import { cva } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-base font-semibold ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-orange focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
@@ -14,10 +14,8 @@ const buttonVariants = cva(
         input: "bg-white/5 border border-white/10 text-white",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+        default: "w-[170px] h-[62px] px-6",
+        sm: "w-[150px] h-[58px] px-6",
       },
     },
     defaultVariants: {
