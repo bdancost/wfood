6437283e2e82485bf4b5ec96aba87ba9009c90e0
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-02-06 15:16:53 -0300

    Configurando Menu

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import { IoIoArrowRoundForward } from "react-icons/io5";
+import { IoIosArrowRoundForward } from "react-icons/io";
 
 import { motion } from "framer-motion";
 import { fadeIn } from "@/variants";
@@ -31,11 +31,56 @@ const menu = [
 
 const Menu = () => {
   return (
-    <section>
+    <section className="relative bg-menu py-12 xl:py-24" id="menu">
       <div className="container mx-auto">
-        <div>
-          <h2>Favorite Menu</h2>
-          <Link href="/">View all</Link>
+        <motion.div
+          variants={fadeIn("left", 0.3)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.2 }}
+          className="mx-auto max-w-[570px] text-center xl:text-right"
+        >
+          <h2 className="mb-3">Favorite Menu</h2>
+          <Link
+            href="/"
+            className="mb-16 flex items-center justify-center text-green xl:justify-end"
+          >
+            View all
+            <IoIosArrowRoundForward className="text-3xl" />
+          </Link>
+        </motion.div>
+        {/* menu grid */}
+        <div className="grid grid-cols-1 gap-[30px] md:grid-cols-3 md:gap-[15px] xl:grid-cols-4">
+          {menu.map((item, index) => {
+            return (
+              <div
+                key={index}
+                className="group mx-auto max-w-[270px] bg-white shadow-primary xl:mx-0"
+              >
+                {/* img */}
+                <div className="overflow-hidden">
+                  <Image
+                    src={item.img}
+                    width={270}
+                    height={270}
+                    alt="menu"
+                    className="transition-all duration-300 group-hover:scale-110"
+                  />
+                </div>
+                {/* title & price */}
+                <div className="px-[30px] pb-[28px] pt-[20px]">
+                  <Link href="/">
+                    <h3 className="mb-[14px] font-poppins text-black">
+                      {item.title}
+                    </h3>
+                  </Link>
+                  <div className="font-poppins text-xl font-semibold text-orange">
+                    {item.price}
+                  </div>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
